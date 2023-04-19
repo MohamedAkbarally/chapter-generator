c9bab89dd0e0a4e7c975f9be097f1791e0e0e9a0
Mohamed <mohamed@Mohameds-MacBook-Pro-2.local>
2023-04-19 22:18:16 +1000

    fixed react env variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './index.css'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
